a894173f59681b342f28b92b0f734f8eee561013
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:51:13 +0530

    Create 2112-MinimumDifferenceBetweenHighestAndLowestOfKScores

---
+++ 2112-MinimumDifferenceBetweenHighestAndLowestOfKScores/2112-MinimumDifferenceBetweenHighestAndLowestOfKScores.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:51:12 AM
+class Solution {
+    public int minimumDifference(int[] nums, int k) {
+        if(k==1){
+            return 0;
+        }
+        Arrays.sort(nums);
+        int min=Integer.MAX_VALUE;
+        for(int i=0;i+k-1<nums.length;i++){
+            min=Math.min(min,nums[i+k-1]-nums[i]);
+        }
+        return min;
+    }
+}
