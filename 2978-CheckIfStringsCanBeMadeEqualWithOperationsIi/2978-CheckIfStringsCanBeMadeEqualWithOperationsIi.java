9a0b0dcc9bc2a7f7929edd48f0970a369a2cd1cb
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:50:21 +0530

    Create 2978-CheckIfStringsCanBeMadeEqualWithOperationsIi

---
+++ 2978-CheckIfStringsCanBeMadeEqualWithOperationsIi/2978-CheckIfStringsCanBeMadeEqualWithOperationsIi.java
@@ -0,0 +1,16 @@
+// Last updated: 7/9/2026, 9:50:21 AM
+class Solution {
+    public boolean checkStrings(String s1, String s2) {
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+        int[] count1 = new int[256];
+        int[] count2 = new int[256];
+        for (int i = 0; i < s1.length(); i++) {
+            int offset = (i & 1) << 7;
+            count1[offset + s1.charAt(i)]++;
+            count2[offset + s2.charAt(i)]++;
+        }
+        return Arrays.equals(count1, count2);
+    }
+}
