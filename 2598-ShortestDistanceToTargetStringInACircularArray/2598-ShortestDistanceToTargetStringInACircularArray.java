48dce5b87b14b057bc75a4776eccaef911fdde7f
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:50:41 +0530

    Create 2598-ShortestDistanceToTargetStringInACircularArray

---
+++ 2598-ShortestDistanceToTargetStringInACircularArray/2598-ShortestDistanceToTargetStringInACircularArray.java
@@ -0,0 +1,14 @@
+// Last updated: 7/9/2026, 9:50:40 AM
+class Solution {
+    public int closestTarget(String[] words, String target, int startIndex) {
+        int ans = words.length;
+        int n = words.length;
+        for (int i = 0; i < n; ++i) {
+            if (words[i].equals(target)) {
+                int dist = Math.abs(i - startIndex);
+                ans = Math.min(ans, Math.min(dist, n - dist));
+            }
+        }
+        return ans < n ? ans : -1;
+    }
+}
