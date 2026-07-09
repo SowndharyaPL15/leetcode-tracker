6a28a859de21b174327cdad86489c8e8491e3039
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:50:30 +0530

    Create 2766-FindThePrefixCommonArrayOfTwoArrays

---
+++ 2766-FindThePrefixCommonArrayOfTwoArrays/2766-FindThePrefixCommonArrayOfTwoArrays.java
@@ -0,0 +1,20 @@
+// Last updated: 7/9/2026, 9:50:29 AM
+class Solution {
+    public int[] findThePrefixCommonArray(int[] A, int[] B) {
+        int n = A.length;
+        int[] prefixCommonArray = new int[n];
+        for (int currentIndex = 0; currentIndex < n; ++currentIndex) {
+            int commonCount = 0;
+            for (int aIndex = 0; aIndex <= currentIndex; ++aIndex) {
+                for (int bIndex = 0; bIndex <= currentIndex; ++bIndex) {
+                    if (A[aIndex] == B[bIndex]) {
+                        ++commonCount;
+                        break; 
+                    }
+                }
+            }
+            prefixCommonArray[currentIndex] = commonCount;
+        }
+        return prefixCommonArray;
+    }
+}
