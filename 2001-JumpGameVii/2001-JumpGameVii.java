effa575bd5b771e3f93e68607edf593d8151b88c
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:51:26 +0530

    Create 2001-JumpGameVii

---
+++ 2001-JumpGameVii/2001-JumpGameVii.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:51:25 AM
+class Solution {
+    public boolean canReach(String s, int minJ, int maxJ) {
+        int n = s.length();
+        if (s.charAt(n - 1) == '1')
+            return false;
+        int[] dp = new int[n];
+        dp[0] = 1;
+        int reach = 0, maxR = maxJ;
+        for (int i = minJ; i < n; i++) {
+            if (i > maxR) return false;
+            reach += dp[i - minJ];
+            if (i > maxJ) reach -= dp[i - maxJ - 1];
+            if (reach > 0 && s.charAt(i) == '0') {
+                dp[i] = 1;
+                maxR = i + maxJ;
+            }
+        }
+        return reach > 0;
+    }
+}
