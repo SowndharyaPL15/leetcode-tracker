9d332cbc3b42f437c0b1551fe4065044be242b5f
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:50:19 +0530

    Create 3019-FurthestPointFromOrigin

---
+++ 3019-FurthestPointFromOrigin/3019-FurthestPointFromOrigin.java
@@ -0,0 +1,18 @@
+// Last updated: 7/9/2026, 9:50:18 AM
+class Solution {
+    public int furthestDistanceFromOrigin(String moves) {
+        int L = 0;
+        int R = 0;
+        int B = 0;
+        for (char c : moves.toCharArray()) {
+            if (c == 'L') {
+                L++;
+            } else if (c == 'R') {
+                R++;
+            } else {
+                B++;
+            }
+        }
+        return Math.abs(L - R) + B;
+    }
+}
