0e1f6abbe062a23298a7eb943dadf5afdfda68b4
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:51:35 +0530

    Create 1972-RotatingTheBox

---
+++ 1972-RotatingTheBox/1972-RotatingTheBox.java
@@ -0,0 +1,28 @@
+// Last updated: 7/9/2026, 9:51:34 AM
+class Solution {
+    public char[][] rotateTheBox(char[][] boxGrid) {
+        int m = boxGrid.length;
+        int n = boxGrid[0].length;
+        for (int i = 0; i < m; i++) {
+            int empty = n - 1;
+            for (int j = n - 1; j >= 0; j--) {
+                if (boxGrid[i][j] == '*') {
+                    empty = j - 1;
+                } 
+                else if (boxGrid[i][j] == '#') {
+                    char temp = boxGrid[i][j];
+                    boxGrid[i][j] = boxGrid[i][empty];
+                    boxGrid[i][empty] = temp;
+                    empty--;
+                }
+            }
+        }
+        char[][] res = new char[n][m];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                res[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+        return res;
+    }
+}
