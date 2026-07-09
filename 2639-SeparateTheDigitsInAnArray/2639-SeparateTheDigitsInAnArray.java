da607b23d840278b3aecada5958a92543f4c67ec
SowndharyaPL15 <plsowndharya@gmail.com>
2026-07-09 09:50:38 +0530

    Create 2639-SeparateTheDigitsInAnArray

---
+++ 2639-SeparateTheDigitsInAnArray/2639-SeparateTheDigitsInAnArray.java
@@ -0,0 +1,21 @@
+// Last updated: 7/9/2026, 9:50:37 AM
+class Solution {
+    public int[] separateDigits(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+        for (int x : nums) {
+            List<Integer> tmp = new ArrayList<>();
+            while (x > 0) {
+                tmp.add(x % 10);
+                x /= 10;
+            }
+            for (int i = tmp.size() - 1; i >= 0; i--) {
+                res.add(tmp.get(i));
+            }
+        }
+        int[] result = new int[res.size()];
+        for (int i = 0; i < res.size(); i++) {
+            result[i] = res.get(i);
+        }
+        return result;
+    }
+}
