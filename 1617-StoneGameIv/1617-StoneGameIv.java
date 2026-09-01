0ef299aa642b428cbe0caf648f1ac45b341f2c8d
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-01 11:42:47 +0530

    Create 1617-StoneGameIv

---
+++ 1617-StoneGameIv/1617-StoneGameIv.java
@@ -0,0 +1,16 @@
+// Last updated: 9/1/2026, 11:42:47 AM
+class Solution {
+    static final int MAX = 100000;
+    static final boolean[] dp = new boolean[MAX + 1];
+
+    static {
+        for (int i = 0; i <= MAX; i++) {
+            if (dp[i]) continue;
+
+            for (int j = 1; j * j <= MAX - i; j++)
+                dp[i + j * j] = true;
+        }
+    }
+
+    public boolean winnerSquareGame(int n) { return dp[n]; }
+}
