860f09935db7565f742b6144fac8ef67c1275288
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-01 11:35:49 +0530

    Create 3820-NumberOfUniqueXorTripletsIi

---
+++ 3820-NumberOfUniqueXorTripletsIi/3820-NumberOfUniqueXorTripletsIi.java
@@ -0,0 +1,37 @@
+// Last updated: 9/1/2026, 11:35:48 AM
+class Solution {
+
+    public int uniqueXorTriplets(int[] nums) {
+        int n = nums.length;
+        int m = 0;
+        for (int v : nums) {
+            m = Math.max(m, v);
+        }
+        int u = 1;
+        while (u <= m) {
+            u <<= 1;
+        }
+        boolean[] s = new boolean[u];
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                s[nums[i] ^ nums[j]] = true;
+            }
+        }
+        boolean[] t = new boolean[u];
+        for (int x = 0; x < u; x++) {
+            if (!s[x]) {
+                continue;
+            }
+            for (int v : nums) {
+                t[x ^ v] = true;
+            }
+        }
+        int ans = 0;
+        for (boolean b : t) {
+            if (b) {
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
