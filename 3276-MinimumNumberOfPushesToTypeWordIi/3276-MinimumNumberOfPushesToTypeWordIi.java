d97c89bf955f81d39d218f66b6b85ecee6a25cde
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-01 11:38:09 +0530

    Create 3276-MinimumNumberOfPushesToTypeWordIi

---
+++ 3276-MinimumNumberOfPushesToTypeWordIi/3276-MinimumNumberOfPushesToTypeWordIi.java
@@ -0,0 +1,18 @@
+// Last updated: 9/1/2026, 11:38:08 AM
+class Solution {
+    public int minimumPushes(String word) {
+        int[] hashTable = new int[26];
+        for(char ch : word.toCharArray()){
+            hashTable[ch - 'a']++;
+        }
+        int minPushing = 0;
+        Arrays.sort(hashTable);
+        for(int i = 25, pushCnt = 0; i >= 0; i--){
+            int currEleIdx = 25 - i;
+            if(currEleIdx % 8 == 0)
+                pushCnt++;
+            minPushing += hashTable[i] * pushCnt;
+        }
+        return minPushing;
+    }
+}
