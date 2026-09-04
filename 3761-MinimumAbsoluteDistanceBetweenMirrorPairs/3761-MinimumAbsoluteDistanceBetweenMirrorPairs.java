64b8147d7ae97ef3352a35433b20b4c12571ceb3
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-04 10:39:12 +0530

    Create 3761-MinimumAbsoluteDistanceBetweenMirrorPairs

---
+++ 3761-MinimumAbsoluteDistanceBetweenMirrorPairs/3761-MinimumAbsoluteDistanceBetweenMirrorPairs.java
@@ -0,0 +1,24 @@
+// Last updated: 9/4/2026, 10:39:11 AM
+class Solution {
+    public int minMirrorPairDistance(int[] nums) {
+        Map<Integer, Integer> prev = new HashMap<>();
+        int n = nums.length;
+        int ans = n + 1;
+        for (int i = 0; i < n; i++) {
+            int x = nums[i];
+            if (prev.containsKey(x)) {
+                ans = Math.min(ans, i - prev.get(x));
+            }
+            prev.put(reverseNum(x), i);
+        }
+        return ans == n + 1 ? -1 : ans;
+    }
+    private int reverseNum(int x) {
+        int y = 0;
+        while (x > 0) {
+            y = y * 10 + (x % 10);
+            x /= 10;
+        }
+        return y;
+    }
+}
