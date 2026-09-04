8ae3f56e79c9371172f9564503f010ffb65fa61c
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-04 10:47:25 +0530

    Create 3136-ValidWord

---
+++ 3136-ValidWord/3136-ValidWord.java
@@ -0,0 +1,24 @@
+// Last updated: 9/4/2026, 10:47:24 AM
+class Solution {
+    public boolean isValid(String word) {
+        int n=word.length();
+        if(n<3){
+            return false;
+        }
+        int v=0,c=0;
+        for(char ch:word.toCharArray()){
+            if(Character.isLetter(ch)){
+                if("aeiouAEIOU".indexOf(ch) != -1){
+                    v++;
+                }
+                else{
+                    c++;
+                }
+            }
+            else if(!Character.isDigit(ch)){
+                return false;
+            }
+        }
+        return v>=1 && c>=1;
+    }
+}
