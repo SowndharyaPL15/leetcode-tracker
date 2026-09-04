7635961f1f6e0bc844b3e20c21861d502cbbdac1
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-04 10:39:59 +0530

    Create 3751-TotalWavinessOfNumbersInRangeI

---
+++ 3751-TotalWavinessOfNumbersInRangeI/3751-TotalWavinessOfNumbersInRangeI.java
@@ -0,0 +1,22 @@
+// Last updated: 9/4/2026, 10:39:58 AM
+class Solution {
+    public int totalWaviness(int num1, int num2) {
+        int ans = 0;
+        for (int num = num1; num <= num2; num++) {
+            int[] d = new int[10];
+            int len = 0;
+            int temp = num;
+            while (temp > 0) {
+                d[len++] = temp % 10;
+                temp /= 10;
+            }
+            for (int i = 1; i < len - 1; i++) {
+                if ((d[i] > d[i - 1] && d[i] > d[i + 1]) ||
+                    (d[i] < d[i - 1] && d[i] < d[i + 1])) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+}
