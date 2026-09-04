a2f9770aa680f2ee14697fef2f2a84894eaef8be
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-04 10:47:08 +0530

    Create 3185-CountPairsThatFormACompleteDayIi

---
+++ 3185-CountPairsThatFormACompleteDayIi/3185-CountPairsThatFormACompleteDayIi.java
@@ -0,0 +1,12 @@
+// Last updated: 9/4/2026, 10:47:07 AM
+class Solution {
+    public long countCompleteDayPairs(int[] hours) {
+        long ans = 0;
+        int[] count = new int[24];
+        for (int i = 0; i < hours.length; i++) {
+            ans += count[(24 - hours[i] % 24) % 24];
+            count[hours[i] % 24]++;
+        }
+        return ans;
+    }
+}
