fdc0328722c7630eb6519a0bf8f3068e3e6a90f0
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-04 10:39:41 +0530

    Create 3745-MaximizeExpressionOfThreeElements

---
+++ 3745-MaximizeExpressionOfThreeElements/3745-MaximizeExpressionOfThreeElements.java
@@ -0,0 +1,20 @@
+// Last updated: 9/4/2026, 10:39:40 AM
+class Solution {
+    public int maximizeExpressionOfThree(int[] nums) {
+        int m1 = Integer.MIN_VALUE, m2 = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] > m1) {
+                m2 = m1;
+                m1 = nums[i];
+            }
+            else if(nums[i] > m2){
+                 m2 = nums[i];
+            }
+            if(nums[i] < min){ 
+                min = nums[i];
+            }
+        }
+        return m1 + m2 - min;
+    }
+}
