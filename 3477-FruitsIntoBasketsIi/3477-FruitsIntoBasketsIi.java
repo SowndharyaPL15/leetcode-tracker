30ff497460cd6e22cf39d907be7b322a74a36309
SowndharyaPL15 <plsowndharya@gmail.com>
2026-09-04 10:45:10 +0530

    Create 3477-FruitsIntoBasketsIi

---
+++ 3477-FruitsIntoBasketsIi/3477-FruitsIntoBasketsIi.java
@@ -0,0 +1,24 @@
+// Last updated: 9/4/2026, 10:45:10 AM
+class Solution {
+    public int numOfUnplacedFruits(int[] fruits, int[] baskets) {
+        int n = fruits.length;
+        boolean[] used = new boolean[n]; // tracks which baskets are used
+        int unplaced = 0;
+
+        for (int i = 0; i < n; i++) {
+            boolean placed = false;
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+
+        return unplaced;
+    }
+}
