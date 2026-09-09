9076df311674fdd0f66c03556870998041bece2b
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:10:03 +0530

    Create 541-ReverseStringIi

---
+++ 541-ReverseStringIi/541-ReverseStringIi.java
@@ -0,0 +1,18 @@
+// Last updated: 9/9/2026, 2:10:03 PM
+class Solution {
+    public String reverseStr(String s, int k) {
+        char[]a=s.toCharArray();
+       for(int i=0;i<a.length;i+=2*k){
+        int l=i;
+        int r=Math.min(i+k-1,a.length-1);
+        while(l<r){
+            char t=a[l];
+            a[l]=a[r];
+            a[r]=t;
+            l++;
+            r--;
+        }
+       } 
+       return new String(a);
+    }
+}
