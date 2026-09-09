11d9bd03cd8d29576f7deee7e1170e5a88092634
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:09:22 +0530

    Create 709-ToLowerCase

---
+++ 709-ToLowerCase/709-ToLowerCase.java
@@ -0,0 +1,8 @@
+// Last updated: 9/9/2026, 2:09:22 PM
+class Solution {
+    public String toLowerCase(String s) {
+        Scanner x=new Scanner(System.in);
+        String a=s.toLowerCase();
+        return a; 
+    }
+}
