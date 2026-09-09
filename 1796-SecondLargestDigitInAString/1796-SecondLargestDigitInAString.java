780ed7e0acf118bb3cf7d0e805a5562dff4ff360
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:02:29 +0530

    Create 1796-SecondLargestDigitInAString

---
+++ 1796-SecondLargestDigitInAString/1796-SecondLargestDigitInAString.java
@@ -0,0 +1,20 @@
+// Last updated: 9/9/2026, 2:02:28 PM
+class Solution {
+    public int secondHighest(String s) {
+        int sec=-1,max=-1;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch>='0' && ch<='9'){
+               int d= ch-'0';
+               if(d>max){
+                sec=max;
+                max=d;
+               }
+               else if(d<max && d>sec){
+                sec=d;
+               }
+            }
+        }
+        return sec;
+    }
+}
