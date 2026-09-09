65f22a086056cbb9c3aa6abf5b420a58b8da677c
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 12:36:40 +0530

    Create 2078-TwoFurthestHousesWithDifferentColors

---
+++ 2078-TwoFurthestHousesWithDifferentColors/2078-TwoFurthestHousesWithDifferentColors.java
@@ -0,0 +1,15 @@
+// Last updated: 9/9/2026, 12:36:40 PM
+class Solution {
+    public int maxDistance(int[] colors) {
+        int n = colors.length;
+        int res = 0; 
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (colors[i] != colors[j]) {
+                    res = Math.max(res, j - i);
+                }
+            }
+        }
+        return res;
+    }
+}
