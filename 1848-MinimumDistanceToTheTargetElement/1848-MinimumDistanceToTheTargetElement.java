f0d745680ac97d02b28c3ca7bd3e03c57fbd08fb
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:01:45 +0530

    Create 1848-MinimumDistanceToTheTargetElement

---
+++ 1848-MinimumDistanceToTheTargetElement/1848-MinimumDistanceToTheTargetElement.java
@@ -0,0 +1,12 @@
+// Last updated: 9/9/2026, 2:01:45 PM
+class Solution {
+    public int getMinDistance(int[] nums, int target, int start) {
+        int res = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
+                res = Math.min(res, Math.abs(i - start));
+            }
+        }
+        return res;
+    }
+}
