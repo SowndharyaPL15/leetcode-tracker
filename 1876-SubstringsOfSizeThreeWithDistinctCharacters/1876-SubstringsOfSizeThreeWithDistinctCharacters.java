53c5112cea502f344aa760472769b8e099dc1066
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:01:39 +0530

    Create 1876-SubstringsOfSizeThreeWithDistinctCharacters

---
+++ 1876-SubstringsOfSizeThreeWithDistinctCharacters/1876-SubstringsOfSizeThreeWithDistinctCharacters.java
@@ -0,0 +1,16 @@
+// Last updated: 9/9/2026, 2:01:39 PM
+class Solution {
+    public int countGoodSubstrings(String s) {
+        char [] ch=s.toCharArray();
+        int c=0;
+        for(int i=0;i<ch.length-2;i++){
+            char c1=ch[i];
+            char c2=ch[i+1];
+            char c3=ch[i+2];
+            if(c1!=c2 && c1!=c3 && c2!=c3){
+                c++;
+            }
+        }
+        return c;
+    }
+}
