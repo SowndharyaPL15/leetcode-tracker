6cb599cb64a536a8e540e34e984aecc480c1d58e
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:09:13 +0530

    Create 761-SpecialBinaryString

---
+++ 761-SpecialBinaryString/761-SpecialBinaryString.java
@@ -0,0 +1,22 @@
+// Last updated: 9/9/2026, 2:09:13 PM
+class Solution {
+    public String makeLargestSpecial(String s) {
+        int c=0;
+        int i=0;
+        List<String> r = new ArrayList<>();
+        for(int j=0;j<s.length();j++) {
+            if(s.charAt(j)=='1'){
+                 c++;
+            }
+            else{
+                c--;
+            }
+            if(c==0){
+                r.add('1'+makeLargestSpecial(s.substring(i+1,j))+'0');
+                i=j+1;
+            }
+        }
+        Collections.sort(r,Collections.reverseOrder());
+        return String.join("",r);
+    }
+}
