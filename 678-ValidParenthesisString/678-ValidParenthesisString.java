fbe6e683fa0fe650cdcaa50137f02685e2566a8a
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:09:35 +0530

    Create 678-ValidParenthesisString

---
+++ 678-ValidParenthesisString/678-ValidParenthesisString.java
@@ -0,0 +1,24 @@
+// Last updated: 9/9/2026, 2:09:35 PM
+class Solution {
+    public boolean checkValidString(String s) {
+        int low = 0; 
+        int high = 0; 
+        for (char ch : s.toCharArray()) {
+            if (ch == '(') {
+                low++;
+                high++;
+            } else if (ch == ')') {
+                low--;
+                high--;
+            } else { 
+                low--; 
+                high++; 
+            }
+            if (high < 0) {
+                return false;
+            }
+            low = Math.max(low, 0);
+        }
+        return low == 0;
+    }
+}
