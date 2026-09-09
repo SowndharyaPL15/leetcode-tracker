992bdafdb346e8dd77e7af013d1731aed620f46d
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 14:15:04 +0530

    Create 131-PalindromePartitioning

---
+++ 131-PalindromePartitioning/131-PalindromePartitioning.java
@@ -0,0 +1,36 @@
+// Last updated: 9/9/2026, 2:15:03 PM
+class Solution
+{
+    boolean isPalindrome(String s)
+    {
+        String s2 = new StringBuilder(s).reverse().toString();
+        return s.equals(s2);
+    }
+
+    void getAllParts(String s, List<List<String>> ans, List<String> parts)
+    {
+        if (s.length() == 0)
+        {
+            ans.add(new ArrayList<>(parts));  
+            return;
+        }
+        for (int i = 0; i < s.length(); i++)
+        {
+            String part = s.substring(0, i + 1);
+            if (isPalindrome(part))
+            {
+                parts.add(part);
+                getAllParts(s.substring(i + 1), ans, parts);
+                parts.remove(parts.size() - 1);  
+            }
+        }
+    }
+
+    public List<List<String>> partition(String s)
+    {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> parts = new ArrayList<>();
+        getAllParts(s, ans, parts);
+        return ans;
+    }
+}
