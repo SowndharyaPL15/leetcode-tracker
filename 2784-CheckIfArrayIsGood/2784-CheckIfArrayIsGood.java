4308de549fd9bf881ee35e5049b9f5eb2546dde5
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 12:35:12 +0530

    Create 2784-CheckIfArrayIsGood

---
+++ 2784-CheckIfArrayIsGood/2784-CheckIfArrayIsGood.java
@@ -0,0 +1,13 @@
+// Last updated: 9/9/2026, 12:35:11 PM
+class Solution {
+    public boolean isGood(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length - 1;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return false;
+            }
+        }
+        return nums[n] == n;
+    }
+}
