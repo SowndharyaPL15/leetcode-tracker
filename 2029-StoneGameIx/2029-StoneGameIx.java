4116773730b81091c61dfa6c59454e83d3b07a1e
Sowndharya PL <plsowndharya@gmail.com>
2026-09-09 12:36:56 +0530

    Create 2029-StoneGameIx

---
+++ 2029-StoneGameIx/2029-StoneGameIx.java
@@ -0,0 +1,19 @@
+// Last updated: 9/9/2026, 12:36:56 PM
+class Solution {
+    public boolean stoneGameIX(int[] stones) {
+        int a = 0, b = 0, c = 0;
+        for (int x : stones) {
+            if (x % 3 == 0) {
+                a++;
+            } else if (x % 3 == 1) {
+                b++;
+            } else {
+                c++;
+            }
+        }
+        if (a % 2 == 0) {
+            return b > 0 && c > 0;
+        }
+        return Math.abs(b - c) > 2;
+    }
+}
